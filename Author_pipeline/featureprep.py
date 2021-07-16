84c681a2e7b580149c95ea87799373826f075ed4
Saranga7 <saranga.boo@gmail.com>
2021-07-16 20:55:08 +0530

    author pipeline update

--- Author_pipeline/featureprep.py
+++ Author_pipeline/featureprep.py
@@ -6,7 +6,7 @@ from feature_functions import *
 def featureprep_func(df_name):
     df=pd.read_csv(df_name)
     df=add_columns(df)
-    df=df[['index','caps_count','first_token_upper','comma_percent','No_of_tokens','first_letter_upper','Author_Encoded']]
+    df=df[['index','fname','caps_count','first_token_upper','comma_percent','No_of_tokens','first_letter_upper','Author_Encoded']]
     df.to_csv(f'features_{df_name}',index=False)
 
 
@@ -15,14 +15,8 @@ if __name__ == "__main__":
     start=time.perf_counter()
     cmdline_params = {rows[0]:rows[1] for rows in reader(open(sys.argv[1], 'r'))}
 
-    print("Adding features in train set...")
-    featureprep_func(cmdline_params['train_author_encoded'])
-
-    try:
-        print("\nAdding features in test set...")
-        featureprep_func(cmdline_params['test_author_encoded'])
-    except:
-        print("Test set unavailble")
+    print("Adding features in dataset...")
+    featureprep_func(cmdline_params['author_encoded'])
         
     end=time.perf_counter()
     
--- Author_pipeline/split.py
+++ Author_pipeline/split.py
@@ -12,21 +12,25 @@ def train_test_split(df,df_name):
 
     df_test= df[df.fname.isin(test_split)]
     df_test.reset_index(inplace=True,drop=True)
+    df_test=df_test[['index','caps_count','first_token_upper','comma_percent','No_of_tokens','first_letter_upper','Author_Encoded']]
     df_test.to_csv(f'test_{df_name}',index=False)
     print('Test dataframe created!')
 
     df_train=df[~df.fname.isin(test_split)]
     df_train.reset_index(inplace=True,drop=True)
+    df_train=df_train[['index','caps_count','first_token_upper','comma_percent','No_of_tokens','first_letter_upper','Author_Encoded']]
     df_train.to_csv(f'train_{df_name}',index=False)
     print('Train dataframe created!')
 
 if __name__=='__main__':
     print("Split.py running...\n\n")
     start=time.perf_counter()
+
     cmdline_params = {rows[0]:rows[1] for rows in reader(open(sys.argv[1], 'r'))}
-    df_name=cmdline_params['author_encoded']
+    df_name=cmdline_params['features_author_encoded']
     df=pd.read_csv(df_name)
     train_test_split(df,df_name)
+
     end=time.perf_counter()
 
     print(f"Splitting done in {end-start} seconds!\n")
