6f8a588c99ece7f17cb3ef93eb06ec8a855e8cea
Saranga7 <saranga.boo@gmail.com>
2021-08-13 23:44:29 +0530

    Minor changes

--- Train_pipeline/run_all.bat
+++ Train_pipeline/run_all.bat
@@ -3,7 +3,7 @@ python encodings.py inputs.csv
 python featureprep.py inputs.csv
 python split.py inputs.csv
 python additional_features_prep.py inputs.csv
-python train.py inputs.csv yes ANN
+python train.py inputs.csv save ANN
 python predict.py inputs.csv ANN
 python cosine_similarity.py inputs.csv
 python aggregated_cosine_similarity.py inputs.csv
